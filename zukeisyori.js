3a8f6dac12bf234ca295b8e928788fea154d9a43
GameMakingProgramming <ayumi.1110m@gmail.com>
2025-09-07 16:43:43 +0900

    Add files via upload

---
+++ zukeisyori.js
@@ -0,0 +1,77 @@
+const titleV = document.getElementById("titleV");
+const data = document.getElementById("data");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const start = document.getElementById("start");
+const numerator = document.getElementById("numerator");
+const denominator = document.getElementById("denominator");
+
+let URLlist = [
+  "https://www.ap-siken.com/kakomon/21_aki/am1.html",
+  "https://www.ap-siken.com/kakomon/21_aki/am2.html",
+  "https://www.ap-siken.com/kakomon/21_aki/am3.html",
+  "https://www.ap-siken.com/kakomon/21_aki/am4.html",
+  "https://www.fe-siken.com/kakomon/23_haru/am5.html",
+  "https://www.fe-siken.com/kakomon/23_haru/am6.html",
+  "https://www.fe-siken.com/kakomon/23_haru/am7.html",
+  "https://www.fe-siken.com/kakomon/23_haru/am8.html",
+  "https://www.ap-siken.com/kakomon/24_haru/am9.html",
+  "https://www.ap-siken.com/kakomon/24_haru/am10.html",
+  "https://www.ap-siken.com/kakomon/24_haru/am11.html",
+  "https://www.ap-siken.com/kakomon/24_haru/am12.html",
+  "https://www.fe-siken.com/kakomon/20_haru/am13.html",
+  "https://www.fe-siken.com/kakomon/20_haru/am14.html",
+  "https://www.fe-siken.com/kakomon/20_haru/am15.html",
+  "https://www.fe-siken.com/kakomon/20_haru/am16.html",
+  "https://www.ap-siken.com/kakomon/26_haru/am17.html",
+  "https://www.ap-siken.com/kakomon/26_haru/am18.html",
+  "https://www.ap-siken.com/kakomon/26_haru/am19.html",
+  "https://www.ap-siken.com/kakomon/26_haru/am20.html"
+];
+let titleList = [
+  "10進数から2進数への変換",
+  "2の補数と剰余",
+  "2進数での10倍の操作",
+  "算術シフトと減算",
+  "回線利用率の計算",
+  "データ転送時間の計算",
+  "伝送時間の計算（伝送効率考慮）",
+  "伝送にかかる時間の計算",
+  "ネットワークアドレスの特定",
+  "ホストアドレスの個数上限",
+  "割り振ってはいけないIPアドレス",
+  "ブロードキャストアドレスの特定",
+  "平均命令実行時間",
+  "コンピュータの性能（MIPS）",
+  "CPUの平均命令実行数",
+  "トランザクションの処理能力",
+  "キャッシュメモリのヒット率",
+  "実効アクセス時間とヒット率",
+  "実効アクセス時間が短い順",
+  "主記憶の実行アクセス時間"
+];
+
+
+
+let number = 1;
+denominator.textContent = titleList.length
+titleV.textContent = titleList[number-1];
+start.setAttribute('href', URLLlist[number-1]);
+
+left.onclick = () =>{
+    if (number > 1) {
+        number--;
+        numerator.textContent = number;
+        titleV.textContent = titleList[number-1];
+        start.setAttribute('href', URLLlist[number-1]);
+    }
+}
+right.onclick = () =>{
+    if (number < denominator.textContent) {
+        number++;
+        numerator.textContent = number;
+        titleV.textContent = titleList[number-1];
+        start.setAttribute('href', URLLlist[number-1]);
+    }
+}
+
